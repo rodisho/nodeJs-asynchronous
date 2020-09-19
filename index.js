c8c5f55d75f835f13f8363e9763cb53903059397
rodisho <rafiodisho@gmail.com>
2020-09-19 12:13:10 -0400

    Refactor all nested callback to follow flat chain of structured promises

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const fs = require('fs');
 const superagent = require('superagent');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log('Random image saved to the file');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePro = (file) => {
+  return new Promise((resolve, rejects) => {
+    fs.readFile(file, (err, data) => {
+      if (err) rejects('I could not find that file :( .......... ');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, rejects) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) rejects('Could not write file :( ............');
+      resolve('Success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random image saved to the file');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
